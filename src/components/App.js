c05a1d6fbd7fcc205e901b6c291d0084f261f336
So-Yeon Ahn <sozygn@gmail.com>
2023-11-21 15:00:22 +0900

    feat: 최신순, 별점순 정렬기능 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import items from '../mock.json';
 import { ReviewList } from './ReviewList';
 
 export function App() {
+  const [order, orderSet] = useState();
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => {
+    orderSet('createdAt');
+  };
+  const handleBestClick = () => {
+    orderSet('rating');
+  };
+
   return (
     <div>
+      <button onClick={handleNewestClick}>최신순</button>
+      <button onClick={handleBestClick}>별점순</button>
       <ReviewList items={items} />
     </div>
   );
